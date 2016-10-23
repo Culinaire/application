c3f2187462ee30e48bec7111365894f490cccb3c
Frank McCoy <frankmccoy.d@gmail.com>
2016-10-23 09:52:24 -0500

    make link

--- src/bistro/food/resources/views/recipes/index.blade.php
+++ src/bistro/food/resources/views/recipes/index.blade.php
@@ -16,13 +16,15 @@
     <tbody>
       @foreach($recipes as $recipe)
         <tr>
+          
           <td>{{ $recipe->id }}</td>
-          <td>{{ $recipe->name }}</td>
+          <td><a href="{{ route('recipes.show', ['id'=> $recipe->id]) }}">{{ $recipe->name }}</a></td>
           <td>{{ $recipe->slug }}</td>
           <td>{{ $recipe->type }}</td>
           <td>{{ $recipe->file }}</td>
           <td><a class="btn btn-primary btn-block" href="{{ route('recipes.show', ['id'=> $recipe->id]) }}"><span class="fa fa-eye"></span> View</a></td>
           <td><a class="btn btn-primary btn-block" href="{{ route('recipes.edit', ['id'=> $recipe->id]) }}"><span class="fa fa-pencil"></span> Edit</a></td>
+          
         </tr>
       @endforeach
     </tbody>
